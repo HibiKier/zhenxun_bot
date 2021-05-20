d4ba59a2a0be8c8dadb353e62d078799f611be1e
HibiKier <45528451+HibiKier@users.noreply.github.com>
2021-05-20 20:12:49 +0800

    Update server_ip.py

--- plugins/server_ip.py
+++ plugins/server_ip.py
@@ -24,9 +24,9 @@ async def _(bot: Bot, event: PrivateMessageEvent, state: T_State):
 # async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
 #     if event.group_id == 698279647:
 #         await server_ip.finish("嗨呀！当前服务器地址是:"
-#                                "\ncsgo:\n\tay: 121.40.195.22\n\twzt: 101.200.199.143\n我的世界:\n\t47.111.1.220：25565")
+#                                "\ncsgo:***********")
 #     elif event.group_id == 1046451860:
-#         await server_ip.finish("嗨呀！当前服务器地址是:\n121.40.195.22\n !diy")
+#         await server_ip.finish("嗨呀！当前服务器地址是:*********** !diy")
 #     else:
 #         await server_ip.finish("不好意思呀，小真寻不能为你使用此功能，因为服务器IP是真寻的小秘密呀！", at_sender=True)
 
