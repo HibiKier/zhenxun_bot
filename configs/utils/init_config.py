5325bc3e32234a798c5510fcd65668fee4cc94c9
HibiKier <45528451+HibiKier@users.noreply.github.com>
2021-09-09 16:39:03 +0800

    Update init_config.py

--- configs/utils/init_config.py
+++ configs/utils/init_config.py
@@ -1,4 +1,3 @@
-from configs.path_config import DATA_PATH
 from pathlib import Path
 try:
     import ujson as json
@@ -6,12 +5,12 @@ except ModuleNotFoundError:
     import json
 
 
-def init_config(plugins2info_dict, plugins2cd_dict, plugins2exists_dict):
+def init_config(plugins2info_dict, plugins2cd_dict, plugins2exists_dict, DATA_PATH):
     plugins2info_file = Path(DATA_PATH) / 'configs' / 'plugins2info.json'
     plugins2info_file.parent.mkdir(exist_ok=True, parents=True)
 
     if plugins2info_file.exists():
-        with open(plugins2info_file, 'r') as f:
+        with open(plugins2info_file, 'r', encoding='utf8') as f:
             _data = json.load(f)
             for p in plugins2info_dict:
                 if not _data.get(p):
