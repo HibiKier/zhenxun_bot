4c34ac570f32383c5aa9196328847019473b73a4
Dolen <87627379+HiDolen@users.noreply.github.com>
2022-05-29 23:13:36 +0800

    修复抽卡插件：方舟更新UP池信息时，若公告的第一个池子过期会导致无法更新UP池信息

--- plugins/draw_card/handles/prts_handle.py
+++ plugins/draw_card/handles/prts_handle.py
@@ -288,7 +288,8 @@ class PrtsHandle(BaseHandle[Operator]):
                             up_chars.append(
                                 UpChar(name=name, star=star, limited=False, zoom=zoom)
                             )
-                    break  # 这里break会导致个问题：如果一个公告里有两个池子，会漏掉下面的池子，比如 5.19 的定向寻访。但目前我也没啥好想法解决
+                    if start_time <= datetime.now() <= end_time: # 跳过过时的卡池
+                        break  # 这里break会导致个问题：如果一个公告里有两个池子，会漏掉下面的池子，比如 5.19 的定向寻访。但目前我也没啥好想法解决
             if title and start_time and end_time:
                 if start_time <= datetime.now() <= end_time:
                     self.UP_EVENT = UpEvent(
