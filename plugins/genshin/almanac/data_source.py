8309b511c8a204bb5665ef9c077cac7671c2d368
McElaga <1728903318@qq.com>
2022-06-07 15:07:57 +0800

    commit on genshin

--- plugins/genshin/almanac/data_source.py
+++ plugins/genshin/almanac/data_source.py
@@ -1,7 +1,9 @@
 from utils.message_builder import image
 from datetime import datetime
 from pathlib import Path
-from utils.http_utils import AsyncPlaywright
+# from utils.http_utils import AsyncPlaywright
+from utils.browser import get_browser
+from services.log import logger
 from nonebot.adapters.onebot.v11 import MessageSegment
 from typing import Optional
 import os
@@ -21,6 +23,26 @@ async def get_alc_image(path: Path) -> Optional[MessageSegment]:
             file.unlink()
     if f"{date}.png" in os.listdir(path):
         return image(f"{date}.png", "genshin/alc")
-    return await AsyncPlaywright.screenshot(
-        url, path / f"{date}.png", ".GSAlmanacs_gs_almanacs__3qT_A"
-    )
+        
+    page = None
+    try:
+        browser = await get_browser()
+        if not browser:
+            logger.warning("获取 browser 失败，请部署至 linux 环境....")
+            return False
+        page = await browser.new_page()
+        await page.goto(url)
+        await page.wait_for_timeout(2000)
+        await page.locator('//*[@id="next"]/a/button').click()
+        await page.locator('.GSAlmanacs_gs_almanacs__3qT_A').screenshot(path=f"{path}/{date}.png")
+        await page.close()
+        return image(f"{date}.png", "genshin/alc")
+    except Exception as e:
+        logger.error(f"原神黄历更新出错... {type(e)}: {e}")
+        if page:
+            await page.close()
+        return False
+
+    # return await AsyncPlaywright.screenshot(
+    #     url, path / f"{date}.png", ".GSAlmanacs_gs_almanacs__3qT_A"
+    # )
--- plugins/genshin/material_remind/__init__.py
+++ plugins/genshin/material_remind/__init__.py
@@ -55,10 +55,14 @@ async def _(event: MessageEvent):
     if not (IMAGE_PATH / "genshin" / "material" / f"{file_name}.png").exists():
         await update_image()
     await material.send(
+        # Message(
+        #     image(f"{file_name}.png", "genshin/material")
+        #     + "\n※ 每日素材数据来源于 genshin.pub"
+        # )
         Message(
-            image(f"{file_name}.png", "genshin/material")
-            + "\n※ 每日素材数据来源于 genshin.pub"
-        )
+                    image(f"{file_name}.png", "genshin/material")
+                    + "\n※ 数据来源于米游社"
+                )
     )
     logger.info(
         f"(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else 'private'})"
@@ -86,63 +90,66 @@ async def update_image():
         if not browser:
             logger.warning("获取 browser 失败，请部署至 linux 环境....")
             return False
-        url = "https://genshin.pub/daily"
-        page = await browser.new_page()
-        await page.goto(url, wait_until="networkidle", timeout=10000)
-        await page.set_viewport_size({"width": 2560, "height": 1080})
-        await page.evaluate(
-            """
-            document.getElementsByClassName('GSTitleBar_gs_titlebar__2IJqy')[0].remove();
-            e = document.getElementsByClassName('GSContainer_gs_container__2FbUz')[0];
-            e.setAttribute("style", "height:880px");
-        """
-        )
-        await page.click("button")
-        div = await page.query_selector(".GSContainer_content_box__1sIXz")
-        for i, card in enumerate(
-            await page.query_selector_all(".GSTraitCotainer_trait_section__1f3bc")
-        ):
-            index = 0
-            type_ = "char" if not i else "weapons"
-            for x in await card.query_selector_all("xpath=child::*"):
-                await x.screenshot(
-                    path=f"{IMAGE_PATH}/genshin/material/{type_}_{index}.png",
-                    timeout=100000,
-                )
-                # 下滑两次
-                for _ in range(3):
-                    await div.press("PageDown")
-                index += 1
-            # 结束后上滑至顶
-            for _ in range(index * 3):
-                await div.press("PageUp")
-        file_list = os.listdir(f"{IMAGE_PATH}/genshin/material")
-        char_img = [
-            f"{IMAGE_PATH}/genshin/material/{x}"
-            for x in file_list
-            if x.startswith("char")
-        ]
-        weapons_img = [
-            f"{IMAGE_PATH}/genshin/material/{x}"
-            for x in file_list
-            if x.startswith("weapons")
-        ]
-        char_img.sort()
-        weapons_img.sort()
-        height = await asyncio.get_event_loop().run_in_executor(
-            None, get_background_height, weapons_img
-        )
-        background_img = BuildImage(1200, height + 100, color="#f6f2ee")
-        current_width = 50
-        for img_list in [char_img, weapons_img]:
-            current_height = 20
-            for img in img_list:
-                x = BuildImage(0, 0, background=img)
-                background_img.paste(x, (current_width, current_height))
-                current_height += x.size[1]
-            current_width += 600
+        # url = "https://genshin.pub/daily"
+        url = "https://bbs.mihoyo.com/ys/obc/channel/map/193"
+        page = await browser.new_page(viewport={'width': 860, 'height': 3000})
+        await page.goto(url)
+        await page.wait_for_timeout(3000)
+        # await page.set_viewport_size({"width": 2560, "height": 1080})
+        # await page.evaluate(
+        #     """
+        #     document.getElementsByClassName('GSTitleBar_gs_titlebar__2IJqy')[0].remove();
+        #     e = document.getElementsByClassName('GSContainer_gs_container__2FbUz')[0];
+        #     e.setAttribute("style", "height:880px");
+        # """
+        # )
+        # await page.click("button")
+        # div = await page.query_selector(".GSContainer_content_box__1sIXz")
+        # for i, card in enumerate(
+        #     await page.query_selector_all(".GSTraitCotainer_trait_section__1f3bc")
+        # ):
+        #     index = 0
+        #     type_ = "char" if not i else "weapons"
+        #     for x in await card.query_selector_all("xpath=child::*"):
+        #         await x.screenshot(
+        #             path=f"{IMAGE_PATH}/genshin/material/{type_}_{index}.png",
+        #             timeout=100000,
+        #         )
+        #         # 下滑两次
+        #         for _ in range(3):
+        #             await div.press("PageDown")
+        #         index += 1
+        #     # 结束后上滑至顶
+        #     for _ in range(index * 3):
+        #         await div.press("PageUp")
+        # file_list = os.listdir(f"{IMAGE_PATH}/genshin/material")
+        # char_img = [
+        #     f"{IMAGE_PATH}/genshin/material/{x}"
+        #     for x in file_list
+        #     if x.startswith("char")
+        # ]
+        # weapons_img = [
+        #     f"{IMAGE_PATH}/genshin/material/{x}"
+        #     for x in file_list
+        #     if x.startswith("weapons")
+        # ]
+        # char_img.sort()
+        # weapons_img.sort()
+        # height = await asyncio.get_event_loop().run_in_executor(
+        #     None, get_background_height, weapons_img
+        # )
+        # background_img = BuildImage(1200, height + 100, color="#f6f2ee")
+        # current_width = 50
+        # for img_list in [char_img, weapons_img]:
+        #     current_height = 20
+        #     for img in img_list:
+        #         x = BuildImage(0, 0, background=img)
+        #         background_img.paste(x, (current_width, current_height))
+        #         current_height += x.size[1]
+        #     current_width += 600
         file_name = str((datetime.now() - timedelta(hours=4)).date())
-        background_img.save(f"{IMAGE_PATH}/genshin/material/{file_name}.png")
+        # background_img.save(f"{IMAGE_PATH}/genshin/material/{file_name}.png")
+        await page.locator('//*[@id="__layout"]/div/div[2]/div[2]/div/div[1]/div[2]/div/div').screenshot(path=f"{IMAGE_PATH}/genshin/material/{file_name}.png")
         await page.close()
         return True
     except Exception as e:
