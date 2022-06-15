af30285c006311b414f2ae761c52d1e9ec384c4d
HibiKier <45528451+HibiKier@users.noreply.github.com>
2022-06-15 10:08:43 +0800

    Update browser.py

--- utils/browser.py
+++ utils/browser.py
@@ -32,7 +32,7 @@ async def get_browser(**kwargs) -> Browser:
     return _browser or await init(**kwargs)
 
 
-# @driver.on_startup
+@driver.on_startup
 def install():
     """自动安装、更新 Chromium"""
     logger.info("正在检查 Chromium 更新")
