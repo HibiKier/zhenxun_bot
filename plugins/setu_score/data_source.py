a0d59d561e88adb2f74bb790fd25450dcfc7ade6
Sagiri_sense <2214612962@qq.com>
2022-06-16 15:14:48 +0800

    fix: 修复色图评分bug

--- plugins/setu_score/data_source.py
+++ plugins/setu_score/data_source.py
@@ -6,22 +6,23 @@ from configs.config import Config
 
 API_KEY = Config.get_config("setu_score", "API_KEY")
 SECRET_KEY = Config.get_config("setu_score", "SECRET_KEY")
-host = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={API_KEY}&client_secret={SECRET_KEY}'
-response = await AsyncHttpx.get(host)
-access_token = response.json()["access_token"]
-request_url = "https://aip.baidubce.com/rest/2.0/solution/v1/img_censor/v2/user_defined"
-request_url = request_url + "?access_token=" + access_token
-headers = {'content-type': 'application/x-www-form-urlencoded'}
 
 
 async def get_setu_score(setu: str) -> "str,int":
     s_time = time.time()
+
     if not API_KEY:
         return "缺失API_KEY！", 500
     if not SECRET_KEY:
         return "缺失SECRET_KEY！", 500
     params = {"imgUrl": setu}
     try:
+        host = f'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={API_KEY}&client_secret={SECRET_KEY}'
+        response = await AsyncHttpx.get(host)
+        access_token = response.json()["access_token"]
+        request_url = "https://aip.baidubce.com/rest/2.0/solution/v1/img_censor/v2/user_defined"
+        request_url = request_url + "?access_token=" + access_token
+        headers = {'content-type': 'application/x-www-form-urlencoded'}
         resp = await AsyncHttpx.post(request_url, data=params, headers=headers)
         resp_json = resp.json()
         if "error_code" in resp_json:
