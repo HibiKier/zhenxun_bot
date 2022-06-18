da67f1f6f8ec0841e6bed64cade7e895a8351862
HibiKier <775757368@qq.com>
2022-06-18 14:48:46 +0800

    update webui

--- plugins/web_ui/api/plugins.py
+++ plugins/web_ui/api/plugins.py
@@ -1,3 +1,5 @@
+from pydantic import ValidationError
+
 from configs.config import Config
 from services.log import logger
 from utils.manager import (plugins2block_manager, plugins2cd_manager,
@@ -85,6 +87,10 @@ def _(type_: Optional[str], user: User = Depends(token_to_user)) -> Result:
                         id_ += 1
                     data["plugin_config"] = tmp
                 plugin_list.append(Plugin(**data))
+            except (AttributeError, ValidationError) as e:
+                logger.error(
+                    f"WEB_UI GET /webui/plugins model：{model} 发生错误 {type(e)}：{e}"
+                )
             except Exception as e:
                 logger.error(
                     f"WEB_UI GET /webui/plugins model：{model} 发生错误 {type(e)}：{e}"
