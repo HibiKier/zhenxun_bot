25aa6a8774b0942cadfe0f1bdc996246ca09bf19
U2y <103440634+U2yyy@users.noreply.github.com>
2022-09-08 23:52:58 +0800

    Update __init__.py

--- plugins/music/__init__.py
+++ plugins/music/__init__.py
@@ -32,11 +32,13 @@ music_handler = on_command("点歌", priority=5, block=True)
 
 @music_handler.handle()
 async def handle_first_receive(matcher: Matcher, args: Message = CommandArg()):
+    #/Edited by U2yyy/使用matcher来set key值
     if args:
         matcher.set_arg("song_name",args)
         
 @music_handler.got("song_name", prompt="歌名是？")
 async def _(bot: Bot, event: MessageEvent, song_name: Message = Arg()):
+    #/Edited by U2yyy/这里把set或者传入的key值转换为string类型值来供函数使用
     song = song_name.extract_plain_text().strip()
     song_id = await get_song_id(song)
     if not song_id:
