37e00038103cd1a84fbb60fc01abd821efd70c47
HibiKier <45528451+HibiKier@users.noreply.github.com>
2023-02-18 23:03:30 +0800

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -19,7 +19,7 @@ NICKNAME: str = "小真寻"
 
 # 数据库（必要）
 # 如果填写了bind就不需要再填写后面的字段了#）
-# 示例："bind": "postgresql://user:password@127.0.0.1:5432/database"
+# 示例："bind": "postgres://user:password@127.0.0.1:5432/database"
 bind: str = ""  # 数据库连接链接
 sql_name: str = "postgres"
 user: str = ""  # 数据用户名
