1c83c6237e7cdfe6848e67f90fde2219d9630d2d
shenghuo2 <1308357113@qq.com>
2023-04-26 11:15:28 +0800

    delete test message

--- extensive_plugin/qr_scan/__init__.py
+++ extensive_plugin/qr_scan/__init__.py
@@ -24,7 +24,7 @@ usage：
 """.strip()
 __plugin_des__ = "即时解码群聊出现的二维码"
 __plugin_version__ = 0.1
-__plugin_author__ = "Tokeii"
+__plugin_author__ = "Tokeii,shenghuo2"
 __plugin_task__ = {"qrcode": "自动扫码"}
 __plugin_settings__ = {
     "level": 5,
@@ -42,20 +42,14 @@ Config.add_plugin_config(
     default_value=False,
     type=bool,
 )
-# __plugin_block_limit__ = {
-#     "check_type": "group",    # 'private'/'group'/'all'，限制私聊/群聊/全部
-#     "limit_type": "717944543",   # 监听对象，以user_id或group_id作为键来限制，'user'：用户id，'group'：群id
-#     "rst": None,            # 回复的话，为None时不回复，可以添加[at]，[uname]，[nickname]来对应艾特，用户群名称，昵称系统昵称
-#     "status": True          # 此限制的开关状态
-# }
 
 
 temppath= 'qrCode/'
 
 # QR_message = on_message(priority=5, block=False)
 QR_message = on_command("scan", priority=5, block=True)
-@QR_message.handle()
 
+@QR_message.handle()
 async def QR_message_(bot: Bot,event: MessageEvent,state: T_State):
     print("触发qrcode")
     msg = event.json()
@@ -63,12 +57,6 @@ async def QR_message_(bot: Bot,event: MessageEvent,state: T_State):
     print(type(msg))
     # print(str(event.json()))
     if [types['data']['url']  for types in msg['message'] if types['type']=='image']:
-
-    # if "scan" in str(msg):
-        
-
-    # if str(event.message).split(',')[0]=='[CQ:image':
-        # message_temp=str(event.message).split(',')
         url = [types['data']['url']  for types in msg['message'] if types['type']=='image'][0]
         print(url)
 
@@ -81,12 +69,9 @@ async def QR_message_(bot: Bot,event: MessageEvent,state: T_State):
                 f.close()
                 #解码二维码
                 image = cv2.imread(temppath+randomname)
-                # print(temppath+randomname)
                 barcode = pyzbar.decode(image)
                 reader1 = zxing.BarCodeReader()
-                # print(reader1)
                 barcode1 = reader1.decode(temppath+randomname)
-                # print(barcode1)
             if barcode1.format!=None:
                 await QR_message.finish(f"[+] 检测到{barcode1.format}码,已自动帮您解码"+Message("[CQ:face,id=320]")+'\n[+] '+barcode1.raw+'\ndecode by zxing')
             if barcode:
@@ -109,7 +94,7 @@ async def QR_message_(bot: Bot,event: MessageEvent,state: T_State):
         # 删除保存的图片
         try:
             os.remove(temppath+randomname)
-            print("尝试删除")
+            # print("尝试删除")
             # pass
         except Exception as e:
             error = ('错误明细是' + str(e.__class__.__name__) + str(e))
