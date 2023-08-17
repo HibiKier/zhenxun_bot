354643f0f6d93edc0f799d8fadd249fdc74aaddd
HibiKier <45528451+HibiKier@users.noreply.github.com>
2023-08-17 22:37:50 +0800

    Update anti.py

--- plugins/image_management/send_image/anti.py
+++ plugins/image_management/send_image/anti.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 
 
-def pixRandomChange(img):
+def pix_random_change(img):
     # Image转cv2
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
     img[0, 0, 0] = random.randint(0, 0xfffffff)
@@ -14,13 +14,10 @@ def pixRandomChange(img):
     return img
 
 
-def pixRandomChangeFile(path):
+def pix_random_change_file(path: str):
     # 注意：cv2.imread()不支持路径中文
     warnings.filterwarnings("ignore", category=Warning)
     img = cv2.imread(path)
     img[0, 0, 0] = random.randint(0, 0xfffffff)
     cv2.imwrite(path, img)
-    print('finish')
 
-
-pixRandomChangeFile('./0.jpg')
