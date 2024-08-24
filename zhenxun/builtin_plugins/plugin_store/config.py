c089395597b9303e0a395ac695a6d0bc3c4ca31a
HibiKier <45528451+HibiKier@users.noreply.github.com>
2024-08-24 20:04:24 +0800

    Update config.py

--- zhenxun/builtin_plugins/plugin_store/config.py
+++ zhenxun/builtin_plugins/plugin_store/config.py
@@ -4,12 +4,10 @@ BASE_PATH = Path() / "zhenxun"
 BASE_PATH.mkdir(parents=True, exist_ok=True)
 
 
-CONFIG_URL_LIST = [
-    ("https://cdn.jsdelivr.net/gh/HibiKier/zhenxun_bot_plugins/plugins.json",
-     "https://api.github.com/repos/HibiKier/zhenxun_bot_plugins/contents/{}?ref=main")
-]
+CONFIG_URL = "https://cdn.jsdelivr.net/gh/zhenxun-org/zhenxun_bot_plugins/plugins.json"
 """插件信息文件"""
 
-CONFIG_URL = CONFIG_URL_LIST[0][0]
-
-DOWNLOAD_URL = CONFIG_URL_LIST[0][1]
+DOWNLOAD_URL = (
+    "https://api.github.com/repos/zhenxun-org/zhenxun_bot_plugins/contents/{}?ref=main"
+)
+"""插件下载地址"""
