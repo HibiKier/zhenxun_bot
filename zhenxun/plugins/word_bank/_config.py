b48ec3985db4e428444023484730fa40e67ecc21
AkashiCoin <i@loli.vet>
2024-08-26 16:56:56 +0800

    🐛修复词库问答查看全局词条问题+代码优化

--- zhenxun/plugins/word_bank/_config.py
+++ zhenxun/plugins/word_bank/_config.py
@@ -1,24 +1,43 @@
+from enum import Enum
+from pickle import GLOBAL
 from zhenxun.configs.path_config import DATA_PATH
 
 data_dir = DATA_PATH / "word_bank"
 data_dir.mkdir(parents=True, exist_ok=True)
 
+class ScopeType(Enum):
+    """
+    全局、群聊、私聊
+    """
+    GLOBAL = 0
+    GROUP = 1
+    PRIVATE = 2
+
 scope2int = {
-    "全局": 0,
-    "群聊": 1,
-    "私聊": 2,
+    "全局": ScopeType.GLOBAL,
+    "群聊": ScopeType.GROUP,
+    "私聊": ScopeType.PRIVATE,
 }
 
+class WordType(Enum):
+    """
+    精准、模糊、正则、图片
+    """
+    EXACT = 0
+    FUZZY = 1
+    REGEX = 2
+    IMAGE = 3
+
 type2int = {
-    "精准": 0,
-    "模糊": 1,
-    "正则": 2,
-    "图片": 3,
+    "精准": WordType.EXACT,
+    "模糊": WordType.FUZZY,
+    "正则": WordType.REGEX,
+    "图片": WordType.IMAGE,
 }
 
 int2type = {
-    0: "精准",
-    1: "模糊",
-    2: "正则",
-    3: "图片",
+    WordType.EXACT: "精准",
+    WordType.FUZZY: "模糊",
+    WordType.REGEX: "正则",
+    WordType.IMAGE: "图片",
 }
