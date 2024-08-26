28425756e8732274a68b1f7b35f1436951811eb9
AkashiCoin <55268546+AkashiCoin@users.noreply.github.com>
2024-08-26 10:32:21 +0800

    🐛 WebUI初始化逻辑修改

--- zhenxun/builtin_plugins/web_ui/config.py
+++ zhenxun/builtin_plugins/web_ui/config.py
@@ -1,20 +1,6 @@
-import nonebot
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 from strenum import StrEnum
 
-app = nonebot.get_app()
-
 origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 AVA_URL = "http://q1.qlogo.cn/g?b=qq&nk={}&s=160"
 
