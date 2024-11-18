3065a4bf7a497e41d1fcc9b2dd0aeec8f2e28b85
HibiKier <775757368@qq.com>
2024-11-18 10:12:58 +0800

    ✨ webui和数据库页面和查询所有表支持mysql和sqlite

--- zhenxun/builtin_plugins/web_ui/api/tabs/database/__init__.py
+++ zhenxun/builtin_plugins/web_ui/api/tabs/database/__init__.py
@@ -5,12 +5,13 @@ from fastapi import Request, APIRouter
 from fastapi.responses import JSONResponse
 from tortoise.exceptions import OperationalError
 
+from zhenxun.configs.config import BotConfig
 from zhenxun.models.task_info import TaskInfo
 from zhenxun.models.plugin_info import PluginInfo
 
 from .models.sql_log import SqlLog
 from ....utils import authentication
-from .models.model import SqlText, SqlModel
+from .models.model import Column, SqlText, SqlModel
 from ....base_model import Result, QueryModel, BaseResultModel
 
 router = APIRouter(prefix="/database")
@@ -22,18 +23,47 @@ driver: Driver = nonebot.get_driver()
 SQL_DICT = {}
 
 
-SELECT_TABLE_SQL = """
-select a.tablename as name,d.description as desc from pg_tables a
-    left join pg_class c on relname=tablename
-    left join pg_description d on oid=objoid
-    and objsubid=0 where a.schemaname = 'public'
+SELECT_TABLE_MYSQL_SQL = """
+SELECT table_name AS name, table_comment AS `desc`
+FROM information_schema.tables
+WHERE table_schema = DATABASE();
 """
 
-SELECT_TABLE_COLUMN_SQL = """
+SELECT_TABLE_SQLITE_SQL = """
+SELECT name FROM sqlite_master WHERE type='table';
+"""
+
+SELECT_TABLE_PSQL_SQL = """
+select a.tablename as name,d.description as desc from pg_tables a
+    left join pg_class c on relname=tablename
+    left join pg_description d on oid=objoid and objsubid=0 where a.schemaname='public'
+"""
+
+SELECT_TABLE_COLUMN_PSQL_SQL = """
 SELECT column_name, data_type, character_maximum_length as max_length, is_nullable
 FROM information_schema.columns
 WHERE table_name = '{}';
 """
+
+SELECT_TABLE_COLUMN_MYSQL_SQL = """
+SHOW COLUMNS FROM {};
+"""
+
+SELECT_TABLE_COLUMN_SQLITE_SQL = """
+PRAGMA table_info({});
+"""
+
+type2sql = {
+    "mysql": SELECT_TABLE_MYSQL_SQL,
+    "sqlite": SELECT_TABLE_SQLITE_SQL,
+    "postgres": SELECT_TABLE_PSQL_SQL,
+}
+
+type2sql_column = {
+    "mysql": SELECT_TABLE_COLUMN_MYSQL_SQL,
+    "sqlite": SELECT_TABLE_COLUMN_SQLITE_SQL,
+    "postgres": SELECT_TABLE_COLUMN_PSQL_SQL,
+}
 
 
 @driver.on_startup
@@ -71,22 +101,47 @@ async def _():
 )
 async def _() -> Result[list[dict]]:
     db = Tortoise.get_connection("default")
-    query = await db.execute_query_dict(SELECT_TABLE_SQL)
+    sql_type = BotConfig.get_sql_type()
+    query = await db.execute_query_dict(type2sql[sql_type])
     return Result.ok(query)
 
 
 @router.get(
     "/get_table_column",
     dependencies=[authentication()],
-    response_model=Result[list[dict]],
+    response_model=Result[list[Column]],
     response_class=JSONResponse,
     description="获取表字段",
 )
-async def _(table_name: str) -> Result[list[dict]]:
+async def _(table_name: str) -> Result[list[Column]]:
     db = Tortoise.get_connection("default")
-    # print(SELECT_TABLE_COLUMN_SQL.format(table_name))
-    query = await db.execute_query_dict(SELECT_TABLE_COLUMN_SQL.format(table_name))
-    return Result.ok(query)
+    sql_type = BotConfig.get_sql_type()
+    sql = type2sql_column[sql_type]
+    query = await db.execute_query_dict(sql.format(table_name))
+    result_list = []
+    if sql_type == "sqlite":
+        result_list.extend(
+            Column(
+                column_name=result["name"],
+                data_type=result["type"],
+                max_length=-1,
+                is_nullable="YES" if result["notnull"] == 1 else "NO",
+            )
+            for result in query
+        )
+    elif sql_type == "mysql":
+        result_list.extend(
+            Column(
+                column_name=result["Field"],
+                data_type=result["Type"],
+                max_length=-1,
+                is_nullable=result["Null"],
+            )
+            for result in query
+        )
+    else:
+        result_list.extend(Column(**result) for result in query)
+    return Result.ok(result_list)
 
 
 @router.post(
